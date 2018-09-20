15022bff27fd04faee53e9b77e7e9ef5c07f9274
Benjamin Glodeck <glodeckbenjamin@gmail.com>
2018-09-20 20:18:53 +0200

    Basic Android test

---
+++ Assets/Scripts/ColorchangeTEST.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorchangeTEST : MonoBehaviour
+{
+    public Color[] colorBank;
+    private SpriteRenderer s;
+
+    void Start ()
+    {
+        s = GetComponent<SpriteRenderer>();
+        int num = Random.Range(0, colorBank.Length);
+        s.color = colorBank[num];
+	}
+
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int num = Random.Range(0, colorBank.Length);
+            s.color = colorBank[num];
+        }
+    }
+}
