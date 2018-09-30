70fe073f51f61540af1af24f40aed1d7c81d078c
Joel <joelbader@outlook.com>
2018-09-30 17:52:29 +0200

    Added Temp Scripts

---
+++ Assets/Scripts/CGenButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGenButton : MonoBehaviour {
+
+    public GameObject textBox;
+
+    public void ClickTheButton()
+    {
+        CurrencyTest.CurrencyCount += 1;
+    }
+
+
+}
---
+++ Assets/Scripts/CurrencyTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrencyTest : MonoBehaviour {
+
+    public static int CurrencyCount;
+    public GameObject CurrencyDisplay;
+    public int InternalCurrency;
+	
+	void Update () {
+
+        InternalCurrency = CurrencyCount;
+        CurrencyDisplay.GetComponent<Text>().text = "Currency: " + InternalCurrency;
+
+	}
+}
