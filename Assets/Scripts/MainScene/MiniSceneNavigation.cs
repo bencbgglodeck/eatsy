0ec61bf505143791dbe3c7b6c180b731a2113062
Benjamin Glodeck <glodeckbenjamin@gmail.com>
2018-11-04 21:48:03 +0100

    MiniScene Interface fixes

---
+++ Assets/Scripts/MainScene/MiniSceneNavigation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MiniSceneNavigation : MonoBehaviour
+{
+    public void CatGameEnter()
+    {
+        SceneManager.LoadScene("MiniCat");
+    }
+    public void FoodGameEnter()
+    {
+        SceneManager.LoadScene("MiniFood");
+    }
+    public void CookieGameEnter()
+    {
+        SceneManager.LoadScene("MiniCookie");
+    }
+    public void BlockGameEnter()
+    {
+        SceneManager.LoadScene("MiniBlocks");
+    }
+    public void BackEnter()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
