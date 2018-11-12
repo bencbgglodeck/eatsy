bb7730d0b624b6b08dd0aa7173768b63b49cc0dc
Benjamin Glodeck <glodeckbenjamin@gmail.com>
2018-11-12 23:43:12 +0100

    MiniScene Fixes

--- Assets/Scripts/MainScene/MiniSceneNavigation.cs
+++ Assets/Scripts/MainScene/MiniSceneNavigation.cs
@@ -7,22 +7,27 @@ public class MiniSceneNavigation : MonoBehaviour
 {
     public void CatGameEnter()
     {
+        MasterScript.energyCount--;
         SceneManager.LoadScene("MiniCat");
     }
     public void FoodGameEnter()
     {
+        MasterScript.energyCount--;
         SceneManager.LoadScene("MiniFood");
     }
     public void CookieGameEnter()
     {
+        MasterScript.energyCount--;
         SceneManager.LoadScene("MiniCookie");
     }
     public void BlockGameEnter()
     {
+        MasterScript.energyCount--;
         SceneManager.LoadScene("MiniBlocks");
     }
     public void BackEnter()
     {
+        MasterScript.energyCount--;
         SceneManager.LoadScene("MainScene");
     }
 }
